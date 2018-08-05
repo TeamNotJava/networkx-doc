2df323022b0524e2f23cf044f1adf76f1c616910
Johannes Schulte <johannes.bernhard.schulte@rwth-aachen.de>
2018-08-05 20:18:39 +0200

    Update doc

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.2rc1.dev20180805165107',
+    VERSION: '2.2rc1.dev20180805181421',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
